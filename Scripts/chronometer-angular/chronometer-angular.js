870fc3f2346f6de20f6bfeb93a54901ea3af3c31
Nicholas Barger <nicholas@nicholasbarger.com>
2015-05-19 16:00:46 -0400

    Updates angular to use ngModel instead of model.

--- Scripts/chronometer-angular/chronometer-angular.js
+++ Scripts/chronometer-angular/chronometer-angular.js
@@ -1,30 +1,31 @@
 ﻿angular.module('chronometer', [])
     .directive('chronometer', function () {
         return {
-            template: function (elem, attrs) {
-                return '<input type="text" id="' + attrs.id + '_inner" ng-model="model" class="chronometer" />'
-            },
+            restrict: 'E',
+            require: '?ngModel',
             scope: {
-                date: '=',
+                id: '=',
                 inputformat: '@',
                 outputformat: '@',
-                includetime: '@',
-                model: '='
+                includetime: '@'
             },
-            link: function (scope, elem, attrs) {
+            template: function (elem, attrs) {
+                return '<input type="text" id="' + attrs.id + '_inner" class="chronometer form-control" />';
+            },
+            link: function (scope, elem, attrs, ngModelCtrl) {
                 var options = {
                     inputFormat: attrs.inputformat,
                     outputFormat: attrs.outputformat,
                     includeTime: (attrs.includetime === "true")
                 };
 
-                scope[attrs.id] = $('#' + attrs.id + '_inner').chronometer(scope.date, options);
+                scope[attrs.id] = $('#' + attrs.id + '_inner').chronometer(ngModelCtrl.$modelValue, options);
 
                 document.addEventListener(attrs.id + "_inner_updated", function (e) {
                     console.log(e);
                     scope.model = e.val;
-                    scope.$apply(); 
+                    scope.$apply();
                 });
             }
-    }
-});
+        }
+    });
